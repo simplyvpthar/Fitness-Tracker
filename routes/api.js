5f5c377c57766ec2f5cf18a5162de8f7249d245d
Vanh Tharavong <67251570+simplyvpthar@users.noreply.github.com>
2020-11-28 12:57:10 -0600

    uploading files

---
+++ routes/api.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const Exercise = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+  Exercise.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Exercise.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Exercise.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Exercise.find({}).limit(7)
+    .then(dbWorkouts => {
+      console.log(dbWorkouts)
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+  Exercise.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = 4000;
+
+const appex = express();
+
+appex.use(logger("dev"));
+
+appex.use(express.urlencoded({ extended: true }));
+appex.use(express.json());
+
+appex.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+appex.use(require("./routes/api.js"));
+appex.use(require("./routes/view.js"));
+
+appex.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
